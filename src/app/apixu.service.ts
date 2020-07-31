f66624aa74b9f1009dbf224629605bd7fc4b4884
lwairore <kwairore@gmail.com>
2020-07-31 09:32:59 +0300

    - Inject HttpClient into the constructor so that i can use it. - Create a method getWeather() that takes a location parameter and makes a GET request to the provided URL.

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -5,6 +5,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApixuService {
+  myApiKey: string = '814b1cc6f53ac582f374a2e1e342c786';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeather(location) {
+    return this.http.get(
+      'http://api.weatherstack.com/current?access_key='+ this.myApiKey +'&query=' + location
+    );
+  }
 }
